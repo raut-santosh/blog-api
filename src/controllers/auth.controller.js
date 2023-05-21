023e5475d1bbccf0b3439352e8f521b3d2050887
santosh raut <rautsantoshtukaram@gmail.com>
2023-05-21 17:30:31 +0530

    feat: added loginroute

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -92,7 +92,7 @@ exports.verifyOtp = async (req,res) => {
           httpOnly: true,
           maxAge: ONE_DAY,
         });
-        return res.status(200).json({msg: 'Login successful',token:token});
+        return res.status(200).json({msg: 'OTP Verified',token:token});
       }else{
         return res.status(400).json({'msg': 'Invalid OTP'})
       }
@@ -124,3 +124,47 @@ exports.resendOtp = async (req, res) => {
     return res.status(500).json({ 'msg': 'Internal server error' });
   }
 };
+
+
+exports.login = async (req,res) => {
+  let user = await User.findOne({email: req.body.email});
+  if (!user) {
+    return res.status(400).json({'msg': 'User does not exist'})
+  }
+  bcrypt.compare(req.body.password,user.password, (err, result) => {
+    if (err) {
+      console.log(err)
+      return res.status(500).json({'msg': 'Internal server error',error:err});
+    }
+    if (result) {      
+      if(user.is_verified === true){
+        const token = jwt.sign(
+          {
+            _id: user._id,
+            idseq: user.idseq,
+            name: user.name,
+            email: user.email,
+            is_verified: user.is_verified,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: "24h",
+          }
+        );
+        const ONE_DAY = 86400000; // in milliseconds
+
+        res.cookie('token', token, {
+          httpOnly: true,
+          maxAge: ONE_DAY,
+        });
+        return res.status(200).json({msg: 'Login successful',token:token});
+      }else{
+        return res.status(400).json({'msg': 'Invalid OTP'})
+      }
+    }else{
+      return res.status(400).json({'msg': 'Invalid Password'})
+    }
+  })
+}
